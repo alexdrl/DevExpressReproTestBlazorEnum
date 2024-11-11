6415315e88b6a9738cdab4a78f28d4f02b0b96da
Alejandro Del Rincón López <alejandro.delrincon@burningbox.es>
2024-11-11 14:57:23 +0100

    Initial commit with added enum

---
+++ Services/WeatherForecastService.cs
@@ -0,0 +1,18 @@
+namespace TestBlazorGridEnumJson.Services {
+    public class WeatherForecastService {
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+    public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate) {
+        var rng = new Random();
+        return Task.FromResult(Enumerable.Range(1, 20).Select(index => new WeatherForecast {
+            Date = startDate.AddDays(index),
+            TemperatureC = rng.Next(-20, 55),
+            Summary = Summaries[rng.Next(Summaries.Length)],
+            DriveType = (DriveType)rng.Next(0, 5)
+        }).ToArray());
+    }
+}
+}
