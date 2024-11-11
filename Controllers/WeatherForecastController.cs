0f9dd4e1cf921a753e25b9477fea28a14f700544
Alejandro Del Rincón López <alejandro.delrincon@burningbox.es>
2024-11-11 15:30:53 +0100

    Make controller break with Json Enums.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Data.ResponseModel;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using TestBlazorGridEnumJson.Services;
+
+namespace TestBlazorGridEnumJson.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly WeatherForecastService _service;
+
+    public WeatherForecastController(WeatherForecastService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("Grid")]
+    public async Task<LoadResult> GetWeatherForecasts(DataSourceLoadOptions loadOptions)
+    {
+        var data1 = await _service.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now.Date));
+
+        var data = data1.Select(x => new WeatherForecast
+        {
+            Date = x.Date,
+            TemperatureC = x.TemperatureC,
+            Summary = x.Summary,
+            DriveType = x.DriveType
+        });
+
+        return DataSourceLoader.Load(data, loadOptions);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using TestBlazorGridEnumJson.Services;
 using TestBlazorGridEnumJson.Components;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,14 +9,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddDevExpressBlazor(options => {
+builder.Services.AddDevExpressBlazor(options =>
+{
     options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
 });
-builder.Services.AddMvc();
+builder.Services.AddMvc()
+     .AddJsonOptions(o => o.JsonSerializerOptions.Converters.Add(item: new JsonStringEnumConverter()))
+    ;
+
 builder.Services.AddSingleton<WeatherForecastService>();
 
+
+// Server Side Blazor doesn't register HttpClient by default
+if (!builder.Services.Any(x => x.ServiceType == typeof(HttpClient)))
+{
+    // Setup HttpClient for server side in a client side compatible fashion
+    builder.Services.AddScoped<HttpClient>(s =>
+    {
+        // Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+        var uriHelper = s.GetRequiredService<NavigationManager>();
+        return new HttpClient
+        {
+            BaseAddress = new Uri(uriHelper.BaseUri)
+        };
+    });
+}
+builder.Services.AddControllers();
+
 var app = builder.Build();
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -27,5 +51,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllers();
 
 app.Run();
